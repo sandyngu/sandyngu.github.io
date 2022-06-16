4317e4be9d614aff7ce29cfcdaf7e4e49c548559
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-06-16 13:51:39 -0400

    Menu Logo Transitions

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -9,13 +9,18 @@ function HeaderBar() {
       document.querySelector('.headerbar__menu-line2').classList.add('headerbar__menu--hide')
       document.querySelector('.headerbar__menu-line3').classList.add('headerbar__menu--hover')
   }
+  function logoLeave() {
+    document.querySelector('.headerbar__menu-line1').classList.remove('headerbar__menu--hover')
+    document.querySelector('.headerbar__menu-line2').classList.remove('headerbar__menu--hide')
+    document.querySelector('.headerbar__menu-line3').classList.remove('headerbar__menu--hover')
+}
 
   return (
     <div className='headerbar'>
         <img src={Logo} className="headerbar__logo" alt="Logo"/>
         <h2 className="headerbar__text">Sandy Nguyen</h2>
-        <div className="headerbar__menu" onMouseOver={()=>logoHover}>
-            <div className="headerbar__menu-logo" onMouseOver={()=>logoHover}>
+        <div className="headerbar__menu">
+            <div className="headerbar__menu-logo" onMouseOver={()=>logoHover()} onMouseLeave={()=>logoLeave()}>
                 <div className="headerbar__menu-line headerbar__menu-line1">———</div>
                 <div className="headerbar__menu-line headerbar__menu-line2">———</div>
                 <div className="headerbar__menu-line headerbar__menu-line3">———</div>
