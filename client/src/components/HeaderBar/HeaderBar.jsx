36d777e3909440c2a110b32220c7ea8b4d003160
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-06-15 23:09:15 -0400

    Font Added, NavBar Logo Created

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Logo from '../../assets/images/logo2.png'
+import Logo from '../../assets/images/logo.png'
 import './headerbar.scss';
 
 function HeaderBar() {
   return (
     <div className='headerbar'>
         <img src={Logo} className="headerbar__logo" alt="Logo"/>
-        Welcome to Sandy Nguyen's Portfolio
+        <h2 className="headerbar__text">Sandy Nguyen</h2>
+        <div className="headerbar__menu">
+            <div className="headerbar__menu-logo">
+                <div className="headerbar__menu-line headerbar__menu-line1">———</div>
+                <div className="headerbar__menu-line headerbar__menu-line2">———</div>
+                <div className="headerbar__menu-line headerbar__menu-line3">———</div>
+            </div>
+        </div>
     </div>
   )
 }
