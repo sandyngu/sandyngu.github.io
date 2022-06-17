7dc360420210789022a4a65fffafcc02a5d773cb
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-06-16 23:03:24 -0400

    Working Transition for Menu Logo Hover and Menu

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -5,20 +5,22 @@ import './headerbar.scss';
 function HeaderBar() {
 
   function logoHover() {
-      document.querySelector('.headerbar__menu-line1').classList.add('headerbar__menu--hover')
+      document.querySelector('.headerbar__menu-line1').classList.add('headerbar__menu--hover1')
       document.querySelector('.headerbar__menu-line2').classList.add('headerbar__menu--hide')
-      document.querySelector('.headerbar__menu-line3').classList.add('headerbar__menu--hover')
+      document.querySelector('.headerbar__menu-line3').classList.add('headerbar__menu--hover2')
+      document.querySelector('.navbar').style.display="block"
   }
   function logoLeave() {
-    document.querySelector('.headerbar__menu-line1').classList.remove('headerbar__menu--hover')
+    document.querySelector('.headerbar__menu-line1').classList.remove('headerbar__menu--hover1')
     document.querySelector('.headerbar__menu-line2').classList.remove('headerbar__menu--hide')
-    document.querySelector('.headerbar__menu-line3').classList.remove('headerbar__menu--hover')
+    document.querySelector('.headerbar__menu-line3').classList.remove('headerbar__menu--hover2')
+    document.querySelector('.navbar').style.display="none"
 }
 
   return (
     <div className='headerbar'>
         <img src={Logo} className="headerbar__logo" alt="Logo"/>
-        <h2 className="headerbar__text">Sandy Nguyen</h2>
+        <h2 className="headerbar__text">sandy nguyen</h2>
         <div className="headerbar__menu">
             <div className="headerbar__menu-logo" onMouseOver={()=>logoHover()} onMouseLeave={()=>logoLeave()}>
                 <div className="headerbar__menu-line headerbar__menu-line1">———</div>
