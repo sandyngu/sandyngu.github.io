30c65c67aefba761765d7c60e2ae9da81084fe9f
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-06-20 22:48:13 -0400

    Changed Some Colours and HeaderBar Sizing

--- client/src/components/AboutMe/AboutMe.jsx
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -5,7 +5,8 @@ import './aboutme.scss';
 function AboutMe() {
   return (
     <div className="aboutme section" id="aboutme">
-      <p className="title" style={{display: 'none'}}>about me</p>
+        <p className="title" style={{display: 'none'}}>about me</p>
+        <h2 className="aboutme__heading">a bit about me.</h2>
     </div>
   )
 }
