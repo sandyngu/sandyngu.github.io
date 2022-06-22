c61e847aa9e77907b1f5ad5a51043d5c6509d353
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-06-22 15:10:49 -0400

    Added Gifs to Heading Texts

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -5,6 +5,7 @@ function Contact() {
   return (
     <div className="contact section" id="contact">
       <p className="title" style={{display: 'none'}}>contact</p>
+      <h2 className="contact__heading">connect with me.</h2>
     </div>
   )
 }
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -4,7 +4,8 @@ import './projects.scss';
 function Projects() {
   return (
     <div className="projects section" id="projects">
-      <p className="title" style={{display: 'none'}}>projects</p>
+        <p className="title" style={{display: 'none'}}>projects</p>
+        <h2 className="projects__heading">things i've made.</h2>
     </div>
   )
 }
