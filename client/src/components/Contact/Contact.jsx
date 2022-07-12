ed3e92262d8bcecebac5fc59c1057579b29d7f82
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-07-12 11:35:39 -0400

    Text Added to Parallax Sections, Waves Added to Projects, Styling to Text

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -63,52 +63,55 @@ function Contact() {
   }
 
   return (
-    <>
-    <div className="parallax parallax4"></div>
-    <div className="contact section" id="contact">
+    <div className="section" id="contact">
         <p className="title" style={{display: 'none'}}>contact</p>
-        <div className="contact__container">
-            <img src={Arrows} className="contact__arrow" alt="Arrow"/>
-            <h2 className="contact__heading">connect with me.</h2>
+        <div className="parallax parallax4">
+            <h2 className="aboutme__section-title aboutme__section-title1">get in touch</h2>
+            <div className="aboutme__section-box"></div>
         </div>
-        <img src={Tulip} className="contact__image" alt="Tulip"/>
-        <div className="contact__button-container">
-          <a href="https://www.linkedin.com/in/sandyngu/" target="_blank" rel="noreferrer">
-            <div className="contact__button contact__button1" onMouseOver={()=>showLinkedIn()} onMouseLeave={()=>hideLinkedIn()}>
-                <img src={LinkedIn} className="contact__button-logo contact__button-logo1" alt="LinkedIn Logo" />
-                <br/><div className="contact__button-text contact__button-text1">linkedin</div>
+        <div className="contact">
+            <div className="contact__container">
+                <img src={Arrows} className="contact__arrow" alt="Arrow"/>
+                <h2 className="contact__heading">connect with me.</h2>
             </div>
-          </a>
-          <a href="mailto:sandy_nguyen@live.ca">
-            <div className="contact__button contact__button2" onMouseOver={()=>showEmail()} onMouseLeave={()=>hideEmail()}>
-                <img src={Email} className="contact__button-logo contact__button-logo2" alt="Email Logo" />
-                <br/><div className="contact__button-text contact__button-text2">e-mail</div>
-            </div>
-          </a>
-          <a href="https://github.com/sandyngu" target="_blank" rel="noreferrer">
-            <div className="contact__button contact__button3" onMouseOver={()=>showGitHub()} onMouseLeave={()=>hideGitHub()}>
-                <img src={GitHub} className="contact__button-logo contact__button-logo3" alt="GitHub Logo" />
-                <br/><div className="contact__button-text contact__button-text3">github</div>
-            </div>
-          </a>
-          <a href={ResumeDoc} target="_blank" rel="noreferrer">
-            <div className="contact__button contact__button4" onMouseOver={()=>showResume()} onMouseLeave={()=>hideResume()}>
-                <img src={Resume} className="contact__button-logo contact__button-logo4" alt="Resume Logo" />
-                <br/><div className="contact__button-text contact__button-text4">resume</div>
-            </div>
-          </a>
-          <a href="https://instagram.com/sandyngu" target="_blank" rel="noreferrer">
-            <div className="contact__button contact__button5" onMouseOver={()=>showSocialMedia()} onMouseLeave={()=>hideSocialMedia()}>
-                <img src={Insta} className="contact__button-logo contact__button-logo5" alt="Instagram Logo" />
-                <br/><div className="contact__button-text contact__button-text5">socials</div>
-            </div>
-          </a>
-            <div className="contact__button">
-hi
+            <img src={Tulip} className="contact__image" alt="Tulip"/>
+            <div className="contact__button-container">
+              <a href="https://www.linkedin.com/in/sandyngu/" target="_blank" rel="noreferrer">
+                <div className="contact__button contact__button1" onMouseOver={()=>showLinkedIn()} onMouseLeave={()=>hideLinkedIn()}>
+                    <img src={LinkedIn} className="contact__button-logo contact__button-logo1" alt="LinkedIn Logo" />
+                    <br/><div className="contact__button-text contact__button-text1">linkedin</div>
+                </div>
+              </a>
+              <a href="mailto:sandy_nguyen@live.ca">
+                <div className="contact__button contact__button2" onMouseOver={()=>showEmail()} onMouseLeave={()=>hideEmail()}>
+                    <img src={Email} className="contact__button-logo contact__button-logo2" alt="Email Logo" />
+                    <br/><div className="contact__button-text contact__button-text2">e-mail</div>
+                </div>
+              </a>
+              <a href="https://github.com/sandyngu" target="_blank" rel="noreferrer">
+                <div className="contact__button contact__button3" onMouseOver={()=>showGitHub()} onMouseLeave={()=>hideGitHub()}>
+                    <img src={GitHub} className="contact__button-logo contact__button-logo3" alt="GitHub Logo" />
+                    <br/><div className="contact__button-text contact__button-text3">github</div>
+                </div>
+              </a>
+              <a href={ResumeDoc} target="_blank" rel="noreferrer">
+                <div className="contact__button contact__button4" onMouseOver={()=>showResume()} onMouseLeave={()=>hideResume()}>
+                    <img src={Resume} className="contact__button-logo contact__button-logo4" alt="Resume Logo" />
+                    <br/><div className="contact__button-text contact__button-text4">resume</div>
+                </div>
+              </a>
+              <a href="https://instagram.com/sandyngu" target="_blank" rel="noreferrer">
+                <div className="contact__button contact__button5" onMouseOver={()=>showSocialMedia()} onMouseLeave={()=>hideSocialMedia()}>
+                    <img src={Insta} className="contact__button-logo contact__button-logo5" alt="Instagram Logo" />
+                    <br/><div className="contact__button-text contact__button-text5">socials</div>
+                </div>
+              </a>
+                <div className="contact__button">
+    hi
+                </div>
             </div>
         </div>
     </div>
-    </>
   )
 }
 
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -4,14 +4,27 @@ import './projects.scss';
 
 function Projects() {
   return (
-    <>
-    <div className="parallax parallax3"></div>
-    <div className="projects section" id="projects">
+    <div className="section" id="projects">
         <p className="title" style={{display: 'none'}}>projects</p>
-        <h2 className="projects__heading">things i've made.</h2>
-        <img src={Flowers} alt="Flowers" className="projects__flowers flowers"/>
+        <div className="parallax parallax3">
+            <h2 className="aboutme__section-title aboutme__section-title1">creations</h2>
+            <div className="aboutme__section-box"></div>
+        </div>
+        <div className="projects">
+            <h2 className="projects__heading">things i've made.</h2>
+            <img src={Flowers} alt="Flowers" className="projects__flowers flowers"/>
+        </div>
+        <div className="projects__wave projects__wave1">
+          <svg viewBox="0 60 500 500" preserveAspectRatio="xMinYMin meet">
+            <path className="projects__wave-svg projects__wave-svg1" d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z"></path>
+          </svg>
+        </div>
+        <div className="projects__wave projects__wave2">
+          <svg viewBox="0 60 500 500" preserveAspectRatio="xMinYMin meet">
+            <path className="projects__wave-svg projects__wave-svg2" d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z"></path>
+          </svg>
+        </div>
     </div>
-    </>
   )
 }
 
