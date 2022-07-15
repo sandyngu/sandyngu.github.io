480f802a27a2c09facb586ff9a9fed003dbf4471
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-07-15 11:56:39 -0400

    Moved Name on Welcome, Faded Flowers on HeaderBar on Scroll Successful

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -5,14 +5,22 @@ import './headerbar.scss';
 
 function HeaderBar(props) {
 
+  var test = document.querySelector('.blur');
+  window.addEventListener('scroll', function(e) {
+    // http://stackoverflow.com/a/28633515/962603
+    var scroll = window.pageYOffset || document.documentElement.scrollTop ||
+                  document.body.scrollTop || 0;
+    test.style.opacity = Math.max(0, Math.min(1, -scroll / 400 + 2));
+  });
+
     return (
       <div className='headerbar'>
           <img src={Logo} className="headerbar__logo" alt="Logo"/>
           <h2 className="headerbar__text" id="header">{props.state.headerTitle}</h2>
-          {/* <div className="headerbar__image-container">
-            <img src={FlowerBorder} className="headerbar__image" alt="Flowers" />
-            <img src={FlowerBorder} className="headerbar__image" alt="Flowers" />
-          </div> */}
+          <div className="headerbar__image-container">
+            <img src={FlowerBorder} className="headerbar__image blur" alt="Flowers" />
+            <img src={FlowerBorder} className="headerbar__image blur" alt="Flowers" />
+          </div>
           <div className="headerbar__menu">
               <div className="headerbar__menu-logo" onClick={()=>props.logoHover()}>
                   <div className="headerbar__menu-line headerbar__menu-line1">———</div>
--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -10,7 +10,7 @@ function Welcome() {
             <img src={Flowers} alt="Flowers" className="welcome__image flowers"/>
             <h1 className="new new1">andy</h1>
             <h1 className="new new2">Nguyen</h1>
-            <h1 className="big big1">S</h1>
+            <h1 className="big">S</h1>
             <div className="welcome__section-text">
                 <h1 className="welcome__title">welcome.</h1>
                 <p className="welcome__maintext"><span className="welcome__maintext-first">Hi! You've managed to stumble upon my personal portfolio─ congratulations.</span> <br/><br/>It's nice to have some company so feel free to take a look around. I created this webpage from scratch so I hope it's somewhat impressive. Keep scrolling down to learn about me and the small projects I've worked on, and don't hesitate to <a href="#contact"><span className="welcome__maintext-second">connect with me!</span></a></p>
