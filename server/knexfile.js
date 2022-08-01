1f41580defa2d57c2207c4362db54ea7e69a489b
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2022-08-01 16:45:50 -0400

    Reverting Changes

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,13 +2,11 @@ module.exports = {
     development: {
       client: "mysql",
       connection: {
-        host: "127.0.0.1",
+        host: "localhost",
         user: "root",
         password: "nursimulator",
         database: "portfolio",
         charset: "utf8",
-        port: '3306',
-        socketPath: '/var/run/mysqld/mysqld.sock',
         insecureAuth: true
       }
     },
